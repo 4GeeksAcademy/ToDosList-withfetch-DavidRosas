117a2769f4dc2a0a57a58cb4145b9b98560d45cf
David Rosas <156865176+davidrosas809@users.noreply.github.com>
2025-01-07 03:56:06 +0000

    Pending delete tasks

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,47 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [newTask, setNewTask] = useState("")
+	const [pendingTasks, setPendingTasks] = useState([])
 	return (
-		<div className="text-center">
-            
+		<div className="text-center my-3">
+			<h1 >ToDos</h1>
+			<div className="d-flex row">
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+				<input type="text" 
+					onChange={(event) => {
+						setNewTask(event.target.value)
+					}}
+
+					onKeyDown={(event) => {
+						if (event.key == "Enter") {
+							setPendingTasks([...pendingTasks, newTask])
+							setNewTask("")
+						}
+					}}
+					
+					value={newTask} className="form-control m-5" 
+				/>
+
+
+				{pendingTasks.map(task =>
+					<div >
+						<p style={{ border: "1px solid black" }} key={task}>{task}</p>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
+// use filter to remove the tasks
+
+
+
